3c1d691ada616ac1cccf5ce117685a1cab35b92a
Erhan Tezcan <erhany96@gmail.com>
2022-04-25 15:37:56 +0300

    todo...

--- components/mainmenu.tsx
+++ components/mainmenu.tsx
@@ -1,13 +1,12 @@
-import { Group, Button, UnstyledButton } from "@mantine/core"
+import { Group, Box, Paper, Title, Text, Overlay } from "@mantine/core"
 import { Dispatch, SetStateAction, useState } from "react"
 import { useSessionContext } from "../context/session"
 import SelectionType from "../types/selection"
-import Layout from "./layout"
-import styles from "../styles/mainmenu.module.scss"
 import SpotiDiff from "./spotidiff"
 import SpotiFind from "./spotifind"
 import SpotiPeek from "./spotipeek"
 import Head from "next/head"
+import type { Icon } from "tabler-icons-react"
 
 type Props = {
   selection: SelectionType
@@ -16,6 +15,32 @@ type Props = {
 const MainMenu = ({ selection, setSelection }: Props) => {
   const { session } = useSessionContext()
 
+  const MenuItem = ({
+    title,
+    description,
+    selectionKey,
+  }: {
+    title: string
+    description: string
+    selectionKey: SelectionType
+  }) => {
+    return (
+      <Box sx={{ position: "relative", width: "50%", minWidth: "300px" }}>
+        <Paper
+          sx={{ cursor: session.isAuthenticated ? "pointer" : undefined, margin: "auto" }}
+          p="lg"
+          shadow="sm"
+          onClick={() => session.isAuthenticated && setSelection(selectionKey)}
+          radius="lg"
+        >
+          <Title>{title}</Title>
+          <Text>{description}</Text>
+          {!session.isAuthenticated && <Overlay color="gray" opacity={0.6} blur={1.2} radius="lg" />}
+        </Paper>
+      </Box>
+    )
+  }
+
   return {
     main: (
       <>
@@ -23,15 +48,21 @@ const MainMenu = ({ selection, setSelection }: Props) => {
           <title>SpotiStuff</title>
         </Head>
         <Group position="center">
-          <Button className={styles["button"]} disabled={!session.user} onClick={() => setSelection("find")}>
-            SpotiFind
-          </Button>
-          <Button className={styles["button"]} disabled={!session.user} onClick={() => setSelection("peek")}>
-            SpotiPeek
-          </Button>
-          <Button className={styles["button"]} disabled={!session.user} onClick={() => setSelection("diff")}>
-            SpotiDiff
-          </Button>
+          <MenuItem
+            title="SpotiFind"
+            description="Search a Spotify track within the playlists of some user. If the track is added in any of the playlists, you will be able to see them."
+            selectionKey="find"
+          />
+          <MenuItem
+            title="SpotiPeek"
+            description="Peek at the audio features of your currently playing track."
+            selectionKey="peek"
+          />
+          <MenuItem
+            title="SpotiDiff"
+            description="Compare playlists of two users, to see if they have the same tracks or not."
+            selectionKey="diff"
+          />
         </Group>
       </>
     ),
